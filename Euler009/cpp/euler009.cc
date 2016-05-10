a969306e9bf75f42f40820ee13b9a14d3135edee
tonton217 <anthony.wonjay.jung@gmail.com>
2016-05-10 22:03:55 +0900

    Updated euler009.cc

--- Euler009/cpp/euler009.cc
+++ Euler009/cpp/euler009.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 int32_t find_special_pythagorean_triplet(uint32_t n) {
   for (uint32_t a = n / 3; a >= 3; --a) {
-    uint32_t b = ((n - a) * (n - a) - (a * a)) / (2 * (n - a));
+    uint32_t b = (n - a) * (n - a) - (a * a) / (2 * (n - a));
     if (a > b) continue;
     uint32_t c = n - a - b;
     if ((a * a + b * b) == (c * c))
